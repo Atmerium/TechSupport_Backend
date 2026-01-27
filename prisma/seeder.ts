30715df7d7b3b771a123d419af2a57ff421fa5c6
Kende28 <yamakawakende676@gmail.com>
2026-01-27 09:29:04 +0100

    Seeding 20260127

--- prisma/seeder.ts
+++ prisma/seeder.ts
@@ -1,14 +1,40 @@
-import { PrismaClient } from "../generated/prisma/client"
 import {faker} from "@faker-js/faker"
 import dotenv from 'dotenv'
-
-const prisma = new PrismaClient()
+import { PrismaClient }  from "../generated/prisma/client";
 
 dotenv.config();
 
+const prisma = new PrismaClient()
+
 console.log("seeding...")
 
+const main = async () => {
+  for (let i = 0; i < 15; i++) {
+    await prisma.users.create({
+      data: {
+        userEmail: faker.internet.email(),
+        userPassword: faker.internet.password(),
+        userRememberMe: faker.datatype.boolean(),
+        userVisible: false
+      }
+    })
 
+    await prisma.components.create({
+      data: {
+        componentName: faker.commerce.productName(),
+        componentDescription: faker.commerce.productDescription(),
+        componentVisible: false
+      }
+    })
+
+    await prisma.advices.create({
+      data: {
+        adviceContent: faker.lorem.text(),
+        adviceVisible: false
+      }
+    })
+  }
+}
 
 main()
   .then(async () => {
